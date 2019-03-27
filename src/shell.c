0c5f6cac51df91c196de90827535b3806e2800d6
Jihun Kim <starlettkim@gmail.com>
2019-03-27 17:36:02 +0900

    Redesigned structure & Rewrited parse_command

--- src/shell.c
+++ src/shell.c
@@ -4,11 +4,91 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
-#include "shell.h"
+#include <ctype.h>
+
+/*
+ * A struct representing a single command.
+ */
+struct Command {
+    char * cmd;
+	char ** argv;
+	unsigned int argc;
+    unsigned int argv_size;
+};
+
+typedef struct Command  Command;
+typedef Command **      CommandVec;
+
+char * read_line(FILE * infile) {
+	char * line;
+	if ( (line = calloc(4096, sizeof(char))) == NULL ) {	// TODO: variable length
+		return NULL;
+	}
+	return fgets(line, sizeof(line), infile);
+}
+
+Command * parse_command(const char * command_string) {
+	Command * command_struct;
+	command_struct = calloc(1, sizeof(Command));
+
+	const char * tok_start;
+	const char * tok_end;
+
+	// Copy cmd.
+	tok_start = command_string;
+	while (isspace(*tok_start) && *tok_start != '\0') {
+		tok_start++;
+	}
+	if (*tok_start == '\0') {	// command_string is a whitespace string or empty string.
+		return NULL;
+	}
+	tok_end = tok_start;
+	while (!isspace(*tok_end) && *tok_end != '\0') {
+		tok_end++;
+	}
+	command_struct->cmd = calloc(tok_end - tok_start + 1, sizeof(char));
+	strncpy(command_struct->cmd, tok_start, tok_end - tok_start);
+
+	// Copy args.
+	command_struct->argc = 0;
+	command_struct->argv_size = 16;
+	command_struct->argv = calloc(command_struct->argv_size, sizeof(char *));
+	while (1) {
+		tok_start = tok_end;
+		while (isspace(*tok_start) && *tok_start != '\0') {
+			tok_start++;
+		}
+		if (*tok_start == '\0') {
+			return command_struct;
+		}
+		tok_end = tok_start;
+		while (!isspace(*tok_end) && *tok_end != '\0') {
+			tok_end++;
+		}
+		command_struct->argv[command_struct->argc] = calloc(tok_end - tok_start + 1, sizeof(char));
+		if (++command_struct->argc == command_struct->argv_size) {
+			command_struct->argv_size *= 2;
+			command_struct->argv = realloc(command_struct->argv, command_struct->argv_size);
+		}
+	}
+}
+
+// TODO:
+CommandVec parse_line(char * line) {
+	char * p_start = line;
+	do {
+		p_start = strtok(p_start, ";");
+	} while ( (p_start = strtok(p_start, ";")) );
+}
+
+// TODO:
+int exec_commands(const CommandVec command) {
+	return 0;
+}
 
 int main(int argc, char * argv[]) {
 
-	// TODO: move this to seperate function
+	// TODO: move this to seperated function
 	FILE * p_infile;
 	if (argc == 1) {
 	    // Interactive mode. Read command from stdin.
@@ -25,12 +105,9 @@ int main(int argc, char * argv[]) {
 	char * line;
 	while ((line = read_line(p_infile)) != NULL
 		&& strcmp(line, "quit") != 0) {
-	    CommandNode * p_cmd;
-	    p_cmd = parse_line(line);
-	    while (p_cmd != NULL) {
-	    	exec_command(p_cmd);
-	    	p_cmd = p_cmd->p_nxt;
-	    }
+	    CommandVec cmd_vec;
+	    cmd_vec = parse_line(line);
+	    exec_commands(cmd_vec);
 	    free(line);
 	}
 
@@ -38,65 +115,3 @@ int main(int argc, char * argv[]) {
 	return 0;
 }
 
-char * read_line(FILE * infile) {
-    char * line;
-    if ( (line = malloc(4096)) == NULL ) {	// TODO: variable length
-    	return NULL;
-    }
-	return fgets(line, sizeof(line), infile);
-}
-
-CommandNode parse_command(const char * cmd_start, const char * cmd_end) {
-	CommandNode command;
-	memset(&command, 0, sizeof(command));
-
-	const char * tok_start;
-	int tok_len;
-
-	// Copy cmd
-	tok_start = cmd_start;
-	while (*tok_start == ' ') {
-		tok_start++;
-		if (tok_start == cmd_start) {	// No binary name
-			return command;
-		}
-	}
-	tok_len	= 0;
-	while (*(tok_start + tok_len) != ' ' && tok_start + tok_len < cmd_end) {
-		tok_len++;
-	}
-	command.cmd = malloc(sizeof(char) * (tok_len + 1));
-	strncpy(command.cmd, tok_start, tok_len);
-
-	// Copy args
-	// TODO: variable length argv
-	command.argv = malloc(sizeof(char *) * 10);
-	int idx_argv = 0;
-	while (tok_start < cmd_end) {
-		while (*tok_start == ' ' && tok_start < cmd_start) {
-			tok_start++;
-		}
-		tok_len = 0;
-		while (*(tok_start + tok_len) != ' ' && tok_start + tok_len < cmd_end) {
-			tok_len++;
-		}
-		command.argv[idx_argv] = malloc(sizeof(char) * (tok_len + 1));
-		strncpy(command.argv[idx_argv], tok_start, tok_len);
-		idx_argv++;
-	}
-
-	return command;
-}
-
-// TODO:
-CommandList parse_line(char * line) {
-	char * p_start = line;
-    do {
-        p_start = strtok(p_start, ";");
-    } while ( (p_start = strtok(p_start, ";")) );
-}
-
-// TODO:
-int exec_command(const CommandNode * command) {
-	return 0;
-}
