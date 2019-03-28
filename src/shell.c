7552a434041acb4b7fb1e180f73c5b7a679da716
Jihun Kim <starlettkim@gmail.com>
2019-03-28 15:18:14 +0900

    Fixed bugs, now can read commands and parse it properly.

--- src/shell.c
+++ src/shell.c
@@ -11,107 +11,138 @@
  */
 struct Command {
     char * cmd;
-	char ** argv;
-	unsigned int argc;
-    unsigned int argv_size;
+    char ** argv;
+    size_t argc;
+    size_t argv_size;
 };
 
 typedef struct Command  Command;
 typedef Command **      CommandVec;
 
+FILE * get_input_stream(char * argv[]) {
+    if (argv[1] == NULL) {
+        // Interactive mode. Read command from stdin.
+        return stdin;
+    } else {
+        // Batch mode. Read command from batch file.
+        return fopen(argv[1], "r");
+    }
+}
+
 char * read_line(FILE * infile) {
-	char * line;
-	if ( (line = calloc(4096, sizeof(char))) == NULL ) {	// TODO: variable length
-		return NULL;
-	}
-	return fgets(line, sizeof(line), infile);
+    char * line;
+    if ( (line = calloc(4096, sizeof(char))) == NULL ) {	// TODO: variable length
+        return NULL;
+    }
+    return fgets(line, sizeof(*line) * 4096, infile);
 }
 
 Command * parse_command(const char * command_string) {
-	Command * command_struct;
-	command_struct = calloc(1, sizeof(Command));
+    Command * command_struct;
+    const char * tok_start;
+    const char * tok_end;
 
-	const char * tok_start;
-	const char * tok_end;
+    command_struct = calloc(1, sizeof(Command));
+    command_struct->argc = 0;
+    command_struct->argv_size = 1;
+    command_struct->argv = calloc(command_struct->argv_size + 1, sizeof(*command_struct->argv));
 
-	// Copy cmd.
-	tok_start = command_string;
-	while (isspace(*tok_start) && *tok_start != '\0') {
-		tok_start++;
-	}
-	if (*tok_start == '\0') {	// command_string is a whitespace string or empty string.
-		return NULL;
-	}
-	tok_end = tok_start;
-	while (!isspace(*tok_end) && *tok_end != '\0') {
-		tok_end++;
-	}
-	command_struct->cmd = calloc(tok_end - tok_start + 1, sizeof(char));
-	strncpy(command_struct->cmd, tok_start, tok_end - tok_start);
+    // Copy cmd.
+    tok_start = command_string;
+    while (isspace(*tok_start) && *tok_start != '\0') {
+        tok_start++;
+    }
+    if (*tok_start == '\0') {	// command_string is a whitespace string or empty string.
+        return NULL;
+    }
+    tok_end = tok_start;
+    while (!isspace(*tok_end) && *tok_end != '\0') {
+        tok_end++;
+    }
+    command_struct->cmd = calloc(tok_end - tok_start + 1, sizeof(char));
+    strncpy(command_struct->cmd, tok_start, tok_end - tok_start);
 
-	// Copy args.
-	command_struct->argc = 0;
-	command_struct->argv_size = 16;
-	command_struct->argv = calloc(command_struct->argv_size, sizeof(char *));
-	while (1) {
-		tok_start = tok_end;
-		while (isspace(*tok_start) && *tok_start != '\0') {
-			tok_start++;
-		}
-		if (*tok_start == '\0') {
-			return command_struct;
-		}
-		tok_end = tok_start;
-		while (!isspace(*tok_end) && *tok_end != '\0') {
-			tok_end++;
-		}
-		command_struct->argv[command_struct->argc] = calloc(tok_end - tok_start + 1, sizeof(char));
-		if (++command_struct->argc == command_struct->argv_size) {
-			command_struct->argv_size *= 2;
-			command_struct->argv = realloc(command_struct->argv, command_struct->argv_size);
-		}
-	}
+    // Copy args.
+    while (1) {
+        tok_start = tok_end;
+        while (isspace(*tok_start) && *tok_start != '\0') {
+            tok_start++;
+        }
+        if (*tok_start == '\0') {
+            return command_struct;
+        }
+        tok_end = tok_start;
+        while (!isspace(*tok_end) && *tok_end != '\0') {
+            tok_end++;
+        }
+        command_struct->argv[command_struct->argc] = calloc(tok_end - tok_start + 1, sizeof(char));
+        strncpy(command_struct->argv[command_struct->argc], tok_start, tok_end - tok_start);
+        command_struct->argv[++command_struct->argc] = NULL;
+        if (command_struct->argc == command_struct->argv_size) {      // Expand argv
+            command_struct->argv_size *= 2;
+            command_struct->argv = realloc(command_struct->argv,
+                                           sizeof(*command_struct->argv) * (command_struct->argv_size + 1));
+        }
+    }
 }
 
-// TODO:
 CommandVec parse_line(char * line) {
-	char * p_start = line;
-	do {
-		p_start = strtok(p_start, ";");
-	} while ( (p_start = strtok(p_start, ";")) );
+    CommandVec command_vec;
+    size_t command_vec_size;
+    size_t command_vec_cap;
+    char * p;
+
+    command_vec_size = 0;
+    command_vec_cap = 1;
+    command_vec = calloc(command_vec_cap + 1, sizeof(*command_vec));
+    p = strtok(line, ";");
+
+    while (p != NULL) {
+        command_vec[command_vec_size++] = parse_command(p);
+        command_vec[command_vec_size] = NULL;
+        if (command_vec_size == command_vec_cap) {      // Expand command_vec
+            command_vec_cap *= 2;
+            command_vec = realloc(command_vec, sizeof(*command_vec) * (command_vec_cap + 1));
+        }
+        p = strtok(NULL, ";");
+    }
+
+    return command_vec;
 }
 
 // TODO:
-int exec_commands(const CommandVec command) {
-	return 0;
+int exec_commands(const CommandVec command_vec) {
+    int i;
+    for (i = 0; command_vec[i] != NULL; i++) {
+        int j;
+        printf("cmd: %s\n", command_vec[i]->cmd);
+        printf("args: ");
+        for (j = 0; j < command_vec[i]->argc; j++) {
+            printf("%s ", command_vec[i]->argv[j]);
+        }
+        printf("\n");
+    }
+    printf("Done.\n");
+    return 0;
 }
 
 int main(int argc, char * argv[]) {
+    FILE * p_infile;
+    char * line;
 
-	// TODO: move this to seperated function
-	FILE * p_infile;
-	if (argc == 1) {
-	    // Interactive mode. Read command from stdin.
-		p_infile = stdin;
-	} else {
-	    // Batch mode. Read command from batch file.
-		p_infile = fopen(argv[1], "r");
-		if (p_infile == NULL) {
-			fprintf(stderr, "Cannot open batch file: %s\n", argv[1]);
-			exit(1);
-		}
-	}
+    if ((p_infile = get_input_stream(argv)) == NULL) {
+        fprintf(stderr, "Failed to open input stream.\n");
+        exit(1);
+    }
 
-	char * line;
-	while ((line = read_line(p_infile)) != NULL
-		&& strcmp(line, "quit") != 0) {
-	    CommandVec cmd_vec;
-	    cmd_vec = parse_line(line);
-	    exec_commands(cmd_vec);
-	    free(line);
-	}
+    while ((line = read_line(p_infile)) != NULL && strcmp(line, "quit") != 0) {
+        CommandVec cmd_vec;
+        cmd_vec = parse_line(line);
+        exec_commands(cmd_vec);
+        free(line);
+    }
 
-	printf("quit\n");
-	return 0;
+    printf("quit\n");
+    return 0;
 }
 
