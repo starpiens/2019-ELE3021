1d7926bb51ea3ba08b9b529c1acb7f1959634fb7
Jihun Kim <starlettkim@gmail.com>
2019-04-27 23:18:43 +0900

    scheduler update

--- xv6-public/proc.c
+++ xv6-public/proc.c
@@ -15,6 +15,8 @@ struct {
 static struct proc *initproc;
 
 int nextpid = 1;
+int totshare = 0;
+int numdshare = 0;
 extern void forkret(void);
 extern void trapret(void);
 
@@ -325,33 +327,42 @@ scheduler(void)
   struct proc *p;
   struct cpu *c = mycpu();
   c->proc = 0;
-  
+
   for(;;){
     // Enable interrupts on this processor.
     sti();
 
     // Loop over process table looking for process to run.
     acquire(&ptable.lock);
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->state != RUNNABLE)
-        continue;
-
-      // Switch to chosen process.  It is the process's job
-      // to release ptable.lock and then reacquire it
-      // before jumping back to us.
-      c->proc = p;
-      switchuvm(p);
-      p->state = RUNNING;
-
-      swtch(&(c->scheduler), p->context);
-      switchkvm();
-
-      // Process is done running for now.
-      // It should have changed its p->state before coming back.
-      c->proc = 0;
+    struct proc *m = 0;
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++) {
+      if (p->state == RUNNABLE) {
+        if (m == 0 || m->nstep > p->nstep)
+          m = p;
+      }
     }
-    release(&ptable.lock);
+    if (m == 0) {
+      release(&ptable.lock);
+      continue;
+    }
 
+    m->nstep += m->stride;
+
+    // Switch to chosen process.  It is the process's job
+    // to release ptable.lock and then reacquire it
+    // before jumping back to us.
+    c->proc = m;
+    switchuvm(m);
+    m->state = RUNNING;
+
+    swtch(&(c->scheduler), p->context);
+    switchkvm();
+
+    // Process is done running for now.
+    // It should have changed its p->state before coming back.
+    c->proc = 0;
+
+    release(&ptable.lock);
   }
 }
 
--- xv6-public/proc.h
+++ xv6-public/proc.h
@@ -49,6 +49,9 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int share;                   // CPU share of this process
+  unsigned double stride;      // Stride of this process.
+  unsigned long double nstep;  // Number of steps.
 };
 
 // Process memory is laid out contiguously, low addresses first:
